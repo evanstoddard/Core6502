ee6ac1a0012dae1fc44b553945a1ed5a1e42b99c
Evan <evan.stoddard@me.com>
2021-04-12 21:08:45 -0400

    Updated Jump Test Cases

--- tests/Core6502Tests_Jump.cpp
+++ tests/Core6502Tests_Jump.cpp
@@ -19,13 +19,75 @@ public:
 	}
 };
 
-// Can copy this file to make other tests
+// JMP Absolute
 TEST_F(Core6502Tests_Jump, Test_JMP_Absolute) {
-    
 	// Test values
+	uint8_t opCode = 0x4C;
 	uint16_t pcVal = 0x4000;
-	uint16_t addrLB = 0xFE;
-	uint16_t addrUB = 0xCA;
-	
+	uint8_t addrLB = 0xFE;
+	uint8_t addrUB = 0xCA;
+	uint16_t addr = addrLB + (addrUB << 8);
 
+	// Set Memory
+	cpu->registers.PC = pcVal;
+	cpu->mem[pcVal] = addrLB;
+	cpu->mem[pcVal + 1] = addrUB;
+
+	// Perform Instruction
+	cpu->instructions[opCode].instructionFunction(*cpu, cpu->instructions[opCode]);
+
+	// Check test case
+	EXPECT_EQ(cpu->registers.PC, addr);
+}
+
+// JMP Indirect
+TEST_F(Core6502Tests_Jump, Test_JMP_Indirect) {
+	// Test values
+	uint8_t opCode = 0x6C;
+	uint16_t pcVal = 0x4000;
+	uint8_t indirectLB = 0xFE;
+	uint8_t indirectUB = 0xCA;
+	uint8_t addrLB = 0xBE;
+	uint8_t addrUB = 0xBA;
+	uint16_t indirectAddr = indirectLB + (indirectUB << 8);
+ 	uint16_t addr = addrLB + (addrUB << 8);
+
+	// Set Memory
+	cpu->registers.PC = pcVal;
+	cpu->mem[pcVal] = indirectLB;
+	cpu->mem[pcVal + 1] = indirectUB;
+	cpu->mem[indirectAddr] = addrLB;
+	cpu->mem[indirectAddr + 1] = addrUB;
+
+	// Perform Instruction
+	cpu->instructions[opCode].instructionFunction(*cpu, cpu->instructions[opCode]);
+
+	// Check test case
+	EXPECT_EQ(cpu->registers.PC, addr);
+}
+
+// JMP Indirect Bug
+TEST_F(Core6502Tests_Jump, Test_JMP_Indirect_Bug) {
+	// Test values
+	uint8_t opCode = 0x6C;
+	uint16_t pcVal = 0x4000;
+	uint8_t indirectLB = 0xFF;
+	uint8_t indirectUB = 0xCA;
+	uint8_t addrLB = 0xBE;
+	uint8_t addrUB = 0xBA;
+	uint16_t indirectAddr = indirectLB + (indirectUB << 8);
+ 	uint16_t addr = addrLB + (addrUB << 8);
+
+	// Set Memory
+	cpu->registers.PC = pcVal;
+	cpu->mem[pcVal] = indirectLB;
+	cpu->mem[pcVal + 1] = indirectUB;
+	cpu->mem[indirectAddr] = addrLB;
+	cpu->mem[indirectAddr & 0xFF00] = addrUB;
+
+	// Perform Instruction
+	cpu->instructions[opCode].instructionFunction(*cpu, cpu->instructions[opCode]);
+
+	// Check test case
+	EXPECT_EQ(cpu->registers.PC, addr);
 }
